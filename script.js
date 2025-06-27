b5d4f3544f65273247db810672f5cd7db09ad920
SaisatwikBiku <bsaisatwik@yahoo.com>
2025-06-27 19:21:29 -0400

    done with hero section

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing-text", {
+    strings: ["Web Developer", "AI/ ML Enthusiast", "Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
