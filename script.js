320e697836f50814c09fa1dad31a09d935266a95
SaisatwikBiku <bsaisatwik@yahoo.com>
2025-06-28 00:10:24 -0400

    added about section

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing-text", {
-    strings: ["Web Developer", "AI/ ML Enthusiast", "Software Engineer"],
+    strings: ["Web Developer", "AI/ ML Enthusiast", "Python Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
